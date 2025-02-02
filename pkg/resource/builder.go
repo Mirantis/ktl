2ba11807d7ea24c19faa633fdd1ad29d7a8b2f76
ivc <ivc@users.noreply.github.com>
2025-02-02 15:19:11 +0400

    Add resource.Builder

---
+++ pkg/resource/builder.go
@@ -0,0 +1,65 @@
+package resource
+
+import (
+	"slices"
+
+	"github.com/Mirantis/rekustomize/pkg/types"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+type Builder struct {
+	path  types.NodePath
+	nodes []*yaml.RNode
+
+	hit, miss int
+}
+
+func NewBuilder(root *yaml.RNode) *Builder {
+	return &Builder{nodes: []*yaml.RNode{root}}
+}
+
+func (b *Builder) Build() *yaml.RNode {
+	return b.nodes[0]
+}
+
+func (b *Builder) skipCommon(path types.NodePath) (*yaml.RNode, types.NodePath) {
+	common := 0
+	for i := range min(len(path), len(b.path)) {
+		if path[i] != b.path[i] {
+			break
+		}
+		common++
+	}
+
+	b.path = slices.Clone(path)
+	padding := slices.Repeat([]*yaml.RNode{nil}, len(path[common:]))
+	b.nodes = append(b.nodes[:1+common], padding...)
+
+	if b.nodes[common] == nil {
+		b.miss++
+		return b.nodes[0], path
+	}
+	if common > 0 {
+		b.hit++
+	}
+	return b.nodes[common], path[common:]
+}
+
+func (b *Builder) Add(path types.NodePath, kind yaml.Kind) (*yaml.RNode, error) {
+	root, sub := b.skipCommon(path)
+	rn, err := root.Pipe(yaml.LookupCreate(kind, sub...))
+	if err != nil {
+		return nil, err
+	}
+	b.nodes[len(path)] = rn
+	return rn, nil
+}
+
+func (b *Builder) Set(path types.NodePath, node *yaml.Node) (*yaml.RNode, error) {
+	rn, err := b.Add(path, node.Kind)
+	if err != nil {
+		return nil, err
+	}
+	rn.SetYNode(node)
+	return rn, nil
+}
---
+++ pkg/resource/builder_test.go
@@ -0,0 +1,90 @@
+package resource
+
+import (
+	"testing"
+
+	"github.com/Mirantis/rekustomize/pkg/types"
+	"github.com/google/go-cmp/cmp"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+func TestBuilder(t *testing.T) {
+	// REVISIT: split for more robust hit/miss testing
+	items := []struct {
+		path  types.NodePath
+		kind  yaml.Kind
+		value *yaml.Node
+	}{
+		{
+			types.NodePath{"a"}, 0,
+			yaml.NewMapRNode(nil).YNode(),
+		},
+		{
+			types.NodePath{"a", "b"}, 0,
+			yaml.NewStringRNode("v1").YNode(),
+		},
+		{
+			types.NodePath{"x", "y", "z"},
+			yaml.SequenceNode,
+			nil,
+		},
+		{
+			types.NodePath{"x", "y", "z", "[name=u]"},
+			yaml.MappingNode,
+			nil,
+		},
+		{
+			types.NodePath{"x", "y", "z", "[name=u]", "v"},
+			0,
+			yaml.NewStringRNode("v2").YNode(),
+		},
+		{
+			types.NodePath{"x", "y", "z", "[name=w]"},
+			yaml.MappingNode,
+			nil,
+		},
+		{
+			types.NodePath{"x", "y", "z", "[name=w]", "v"},
+			0,
+			yaml.NewStringRNode("v3").YNode(),
+		},
+		{
+			types.NodePath{"x", "l"},
+			0,
+			yaml.NewStringRNode("out-of-order").YNode(),
+		},
+	}
+	builder := NewBuilder(yaml.NewMapRNode(nil))
+	for _, item := range items {
+		var err error
+		if item.value == nil {
+			_, err = builder.Add(item.path, item.kind)
+		} else {
+			_, err = builder.Set(item.path, item.value)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	got := builder.Build().MustString()
+	want := `a:
+  b: v1
+x:
+  y:
+    z:
+    - name: u
+      v: v2
+    - name: w
+      v: v3
+  l: out-of-order
+`
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("+got -want:\n%s", diff)
+	}
+	if diff := cmp.Diff(5, builder.hit); diff != "" {
+		t.Errorf("hit (+got -want): %s", diff)
+	}
+	if diff := cmp.Diff(1, builder.miss); diff != "" {
+		t.Errorf("miss (+got -want): %s", diff)
+	}
+}
