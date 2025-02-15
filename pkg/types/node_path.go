1af92fc6d5447921511152aa54ac2a910e9356a0
ivc <ivc@users.noreply.github.com>
2025-02-15 14:37:28 +0400

    Add NodePath.Normalize

--- pkg/types/node_path.go
+++ pkg/types/node_path.go
@@ -1,13 +1,12 @@
 package types
 
 import (
+	"slices"
 	"strings"
 
 	"sigs.k8s.io/kustomize/kyaml/utils"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
-
-var rfc6901replacer = strings.NewReplacer("~", "~0", "/", "~1")
 
 // NodePath represents YAML node path
 type NodePath []string
@@ -46,4 +45,70 @@ func (p *NodePath) unmarshalScalar(node *yaml.Node) error {
 	}
 	*p = utils.SmarterPathSplitter(path, ".")
 	return nil
+}
+
+func splitPathCondition(pathPart string) (key, cond string, prefix bool) {
+	prefix = strings.HasPrefix(pathPart, "[")
+	suffix := strings.HasSuffix(pathPart, "]")
+
+	if prefix && suffix {
+		return "*", pathPart, false
+	}
+
+	if !prefix && !suffix {
+		return pathPart, "", true
+	}
+
+	if prefix {
+		splitIdx := strings.LastIndex(pathPart, "]")
+		cond = pathPart[:splitIdx+1]
+		key = pathPart[splitIdx+1:]
+	} else {
+		splitIdx := strings.Index(pathPart, "[")
+		key = pathPart[:splitIdx]
+		cond = pathPart[splitIdx:]
+	}
+
+	if suffix && strings.HasPrefix(cond, "[=") {
+		cond = "[" + key + cond[1:]
+		prefix, suffix = true, false
+	}
+
+	return key, cond, prefix
+}
+
+func mergeConditions(left, right string) string {
+	if "" == left {
+		return right
+	}
+	if "" == right {
+		return left
+	}
+	return strings.TrimSuffix(left, "]") + "," + strings.TrimPrefix(right, "[")
+}
+
+func (p NodePath) Normalize() (NodePath, []string, error) {
+	path := slices.Clone(p)
+	conditions := make([]string, len(path))
+	for i := range p {
+		key, cond, prefix := splitPathCondition(path[i])
+		if prefix {
+			path[i] = key
+			conditions[i] = mergeConditions(conditions[i], cond)
+			continue
+		}
+
+		if i == len(p)-1 {
+			nextKey, _, err := yaml.SplitIndexNameValue(cond)
+			if err != nil {
+				return nil, nil, err
+			}
+			path = append(path, nextKey)
+			conditions = append(conditions, "")
+		}
+
+		path[i] = key
+		conditions[i+1] = cond
+	}
+	return path, conditions, nil
 }
--- pkg/types/node_path_test.go
+++ pkg/types/node_path_test.go
@@ -62,14 +62,24 @@ func TestNodePathUnmarshalYAML(t *testing.T) {
 			want:  types.NodePath{"a", "b", "c"},
 		},
 		{
-			name:  "json-pointer",
-			input: `got: /a/b/c`,
+			name:  "text",
+			input: `got: a.b.c`,
 			want:  types.NodePath{"a", "b", "c"},
 		},
 		{
-			name:  "json-pointer-escaped",
-			input: `got: /a/b~01/c~1d`,
-			want:  types.NodePath{"a", "b~1", "c/d"},
+			name:  "condition",
+			input: `got: a.[b=1].c`,
+			want:  types.NodePath{"a", "[b=1]", "c"},
+		},
+		{
+			name:  "glob",
+			input: `got: a.*.b`,
+			want:  types.NodePath{"a", "*", "b"},
+		},
+		{
+			name:  "mixed",
+			input: `got: a[b=1].*.[c=2]d`,
+			want:  types.NodePath{"a[b=1]", "*", "[c=2]d"},
 		},
 	}
 	for _, test := range tests {
@@ -86,6 +96,70 @@ func TestNodePathUnmarshalYAML(t *testing.T) {
 			}
 			if diff := cmp.Diff(test.want, test.Got); diff != "" {
 				t.Fatalf("-want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestNodePathNormalize(t *testing.T) {
+	tests := []struct {
+		name  string
+		input types.NodePath
+		wantP types.NodePath
+		wantC []string
+		wantE bool
+	}{
+		{
+			name:  "no-conditions",
+			input: types.NodePath{"a", "b", "c"},
+			wantP: types.NodePath{"a", "b", "c"},
+			wantC: []string{"", "", ""},
+		},
+		{
+			name:  "prefix-only",
+			input: types.NodePath{"[a=1]b", "*", "[c=2]d"},
+			wantP: types.NodePath{"b", "*", "d"},
+			wantC: []string{"[a=1]", "", "[c=2]"},
+		},
+		{
+			name:  "glob",
+			input: types.NodePath{"a", "[b=1]", "c"},
+			wantP: types.NodePath{"a", "*", "c"},
+			wantC: []string{"", "", "[b=1]"},
+		},
+		{
+			name:  "overflow",
+			input: types.NodePath{"a", "b[c=1,d=2]"},
+			wantP: types.NodePath{"a", "b", "c"},
+			wantC: []string{"", "", "[c=1,d=2]"},
+		},
+		{
+			name:  "merge",
+			input: types.NodePath{"a[b=1]", "[c=2]d"},
+			wantP: types.NodePath{"a", "d"},
+			wantC: []string{"", "[b=1,c=2]"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotP, gotC, err := test.input.Normalize()
+
+			if err != nil && test.wantE {
+				return
+			}
+			if err != nil {
+				t.Fatalf("want no error, got: %v", err)
+			}
+			if test.wantE {
+				t.Fatalf("want error, got none")
+			}
+
+			if diff := cmp.Diff(test.wantP, gotP); diff != "" {
+				t.Errorf("path -want +got:\n%s", diff)
+			}
+
+			if diff := cmp.Diff(test.wantC, gotC); diff != "" {
+				t.Errorf("conditions -want +got:\n%s", diff)
 			}
 		})
 	}
